90dcc6045996fedf4a59b1ea3ef5ec4178e2dd30
nightknight <shevanx21@gmail.com>
2025-09-29 18:50:25 +0300

    Final version

--- Module-1/Module-1.3/Program.cs
+++ Module-1/Module-1.3/Program.cs
@@ -2,4 +2,4 @@ Console.Write("Введите имя и фамилию: ");
 string[] str = Console.ReadLine()!.Split(' ');
 Console.WriteLine($"Вывод в формате 'Фамилия, Имя': {str[1]}, {str[0]}");
 Console.WriteLine("Нажмите любую клавишу для выхода...");
-Console.ReadKey(); 
+Console.ReadKey();
