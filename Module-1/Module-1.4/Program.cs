5da96db8a1995f91e0a200041a59d71294ff3203
nightknight <shevanx21@gmail.com>
2025-09-29 19:07:08 +0300

    Final version

--- Module-1/Module-1.4/Program.cs
+++ Module-1/Module-1.4/Program.cs
@@ -1,3 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿const int size = 30;
+int[] numbers = new int[size];
+Random rand = new Random();
+// Заполнение массива случайными значениями
+for (int i = 0; i < size; i++)
+    numbers[i] = rand.Next(0, 100); 
+// Вывод массива
+Console.WriteLine("Массив случайных чисел: " + string.Join(" ", numbers));
 
-Console.WriteLine("Hello, World!");
+Array.Sort(numbers); // сортировка по возрастанию
+
+int min = numbers[0];  // Первый элемент
+int max = numbers[^1]; // Последний элемент
+
+Console.WriteLine($"Минимум: {min}");
+Console.WriteLine($"Максимум: {max}");
+
+Console.WriteLine("Нажмите любую клавишу для выхода...");
+Console.ReadKey(); 
