8ea6b48dca424f66ad15d124416b35a8967a6346
nightknight <shevanx21@gmail.com>
2025-09-29 18:31:00 +0300

    Final version Module-1.2.

--- Module-1/Module-1.2/Program.cs
+++ Module-1/Module-1.2/Program.cs
@@ -1,3 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-Console.WriteLine("Hello, Word!");
+﻿Console.Write("Введите радиус круга: ");
+int radius = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Площадь круга равна " + Math.PI * Math.Pow(radius, 2));
+Console.WriteLine("Нажмите любую клавишу для выхода...");
+Console.ReadKey(); 
