43e61fc17af8771efb608976dc088232565bfc34
nightknight <shevanx21@gmail.com>
2025-09-29 19:15:58 +0300

    Final version

--- Module-1/Module-1.5/Program.cs
+++ Module-1/Module-1.5/Program.cs
@@ -1,3 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-Console.WriteLine("Helo,Wrld!");
+﻿Console.Write("Введите возраст: ");
+int age = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(age >= 18 ? "Можно получить водительские права" : "Нельзя получить водительские права");
+Console.WriteLine("Нажмите любую клавишу для выхода...");
+Console.ReadKey(); 
