f269323da7dd358ec309ec2cb9f4d9180c608ac6
nightknight <shevanx21@gmail.com>
2025-09-30 22:04:55 +0300

    Final version

--- Module-1/Module-1.2/Module-1.2.6/Program.cs
+++ Module-1/Module-1.2/Module-1.2.6/Program.cs
@@ -1,3 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-Console.WriteLine("Hello, World!");
+﻿const int size = 10;
+var array = new float[size];
+var indices = new int[size];
+var rand = new Random();
+// Цикл для заполнения случацными вещественными значениями
+for (int i = 0; i < size; i++)
+{
+    array[i] = (float)rand.NextDouble() * 20 - 10; // [0,1) > [0,20) > [-10,10)
+    indices[i] = i; // Начальная нумераия
+}
+// Цикл сортировки индексов по значениям массива
+for (int i = 0; i < size - 1; i++)
+{
+    for (int j = i + 1; j < size; j++)
+    {
+        if (array[indices[i]] > array[indices[j]])
+        {
+            (indices[i], indices[j]) = (indices[j], indices[i]);
+        }
+    }
+}
+Console.WriteLine("Исходный массив: " + string.Join(" ", array.Select(n => n.ToString("F2"))));
+Console.WriteLine("Массив индексов по возрастанию значений: " + string.Join(' ', indices));
+Console.ReadKey();
