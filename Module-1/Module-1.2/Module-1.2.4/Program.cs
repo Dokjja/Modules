3a3c8efb626ab5afced55567984780f19034d00e
nightknight <shevanx21@gmail.com>
2025-09-30 13:56:03 +0300

    Final version

--- Module-1/Module-1.2/Module-1.2.4/Program.cs
+++ Module-1/Module-1.2/Module-1.2.4/Program.cs
@@ -1,3 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите количество элементов: ");
+int K = Convert.ToInt32(Console.ReadLine());
+int[] array =  new int[K];
+var rand = new Random(); // Создание нового объекта класса Random
+Console.Write("Введите значение A: ");
+int A = Convert.ToInt32(Console.ReadLine());    
+Console.Write("Введите значение B: ");
+int B = Convert.ToInt32(Console.ReadLine());
+// Цикл для заполнения массива
+for (int i = 0; i <  K; i++)
+{
+    array[i] = rand.Next(A, B);
+}
+Console.WriteLine(string.Join(", ", array));
+var minIndex = Array.IndexOf(array, array.Min()); // Индекс минимального элемента
+var maxIndex = Array.IndexOf(array, array.Max()); // Индекс максимального элемента
+Console.WriteLine($"Минимальное значение: {array.Min()}, его индекс: {minIndex}\n" +
+                  $"Максимальное значение: {array.Max()}, его индекс: {maxIndex}");
+var firstIndex = Math.Min(minIndex, maxIndex); // Первый индекс по счету
+var secondIndex = Math.Max(minIndex, maxIndex); // второй индекс по счету
+Console.Write("Значения в диапозоне: ");
+for (int i = firstIndex; i <= secondIndex; i++)
+{
+    Console.Write(array[i] + " ");
+}
+Console.ReadKey();
