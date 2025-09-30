31e4931162117c04eaf3effca4f96e4fd4f77bcd
nightknight <shevanx21@gmail.com>
2025-09-30 21:46:10 +0300

    Final version

--- Module-1/Module-1.2/Module-1.2.5/Program.cs
+++ Module-1/Module-1.2/Module-1.2.5/Program.cs
@@ -1,1 +1,22 @@
-﻿
+﻿Console.Write("Введите количество элементов массива: ");
+var K = Convert.ToInt32(Console.ReadLine());
+
+char[] alphabet = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ".ToCharArray(); // Массив всех букв
+char[] vowels = "АЕЁИОУЫЭЮЯ".ToCharArray(); // Массив гласных
+
+Random rand = new Random(); // Создание объекта класса Random
+var array = new char[K]; // Массив для случайных букв
+// Цикл для заполнения массива случайными буквами
+for (int i = 0; i < K; i++)
+    array[i] = alphabet[rand.Next(0, alphabet.Length)]; // Добавление случайной буквы
+
+List<char> constants = new List<char>(K); // Лист для согласных (чтобы меньше кода было)
+// Поиск согласных
+foreach (var letter in array)
+{
+    if(!vowels.Contains(letter))
+        constants.Add(letter);
+}
+Console.WriteLine("Массив случайных букв: " + string.Join(' ', array)); // Вывод массива со случайными буквами
+Console.WriteLine("Лист согласных из массива случайных букв" + string.Join(' ', constants)); // Вывод массива с согласными
+Console.ReadKey();                            
