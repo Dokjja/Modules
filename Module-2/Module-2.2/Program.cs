34b4bb629db1930cd080d71635cbd85796244a29
nightknight <shevanx21@gmail.com>
2025-10-01 11:49:49 +0300

    Final version

--- Module-2/Module-2.2/Program.cs
+++ Module-2/Module-2.2/Program.cs
@@ -1,3 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
 
-Console.WriteLine("Hello, World!");
+class Shape
+{
+    public virtual double Area() => 0;
+    public virtual double Perimeter() => 0;
+    public void Print() => Console.WriteLine($"Площадь: {Area()}, Периметр: {Perimeter()}");
+}
+
+class Circle(double radius): Shape
+{
+    public double Radius { get; set; } =  radius;
+    public override double Area() => Math.PI * Math.Pow(Radius, 2);
+    public override double Perimeter() => Math.PI * Radius * 2;
+}
+
+class Rectangle(double width, double height) : Shape
+{
+    public double Width { get; set; } = width;
+    public double Height { get; set; } = height;
+    public override double Area() => Width * Height;
+    public override double Perimeter() => (2 * Width) + (2 * Height);
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введите радиус для круга: ");
+        double radius = double.Parse(Console.ReadLine()!);
+        var circle = new Circle(radius);
+        circle.Print();
+        Console.Write("Введите ширину прямоугольника: ");
+        var width = double.Parse(Console.ReadLine()!);
+        Console.Write("Введите высоту прямоугольника: ");
+        var height = double.Parse(Console.ReadLine()!);
+        var rectangle = new Rectangle(width, height);
+        rectangle.Print();
+        Console.ReadKey();
+    }
+}
