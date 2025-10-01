c59bb7c5c6ee02c18a58e95624aae5edd9f68850
nightknight <shevanx21@gmail.com>
2025-10-01 08:56:43 +0300

    Final version

--- Module-1/Module-1.3/Module-1.3.2/Program.cs
+++ Module-1/Module-1.3/Module-1.3.2/Program.cs
@@ -1,3 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
 
-Console.WriteLine("Hello, World!");
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите значение максимальной суммы: ");
+        int maxSum = Convert.ToInt32(Console.ReadLine());
+        int sum = 0;
+        var array = new int[1];
+        var temp = new int[maxSum];
+        var count = 0;
+        var rand = new Random();
+        while (sum < maxSum)
+        {
+            int value = rand.Next(1, 10);
+            if (sum + value > maxSum) break;
+            temp[count++] = value;
+            sum += value;
+        }
+        // Создание итогового массива нужной длины
+        var result = new int[count];
+        Array.Copy(temp, result, count);    
+        
+        Console.WriteLine("Сформированный массив: " + string.Join(" ", result) + "\nСумма элементов: " + sum);
+        Console.ReadKey();
+    }
+}
