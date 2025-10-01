2d322f6daaf57eef097922f01a50ed6ea1b2edc1
nightknight <shevanx21@gmail.com>
2025-10-01 08:45:02 +0300

    Final version

--- Module-1/Module-1.3/Module-1.3.1/Program.cs
+++ Module-1/Module-1.3/Module-1.3.1/Program.cs
@@ -1,3 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
 
-Console.WriteLine("Hello, World!");
+class Program
+{
+    static int NOD(int a, int b)
+    {
+        // Алгоритм Евклида
+        while (a != 0 && b != 0)
+        {
+            if (a > b)
+                a %= b;
+                
+            else b %= a;
+        }
+        return a + b;
+    }
+    static void Main(string[] args)
+    {
+        Console.Write("Введите неотрицательный числитель: ");
+        int a = int.Parse(Console.ReadLine()!);
+        Console.Write("Введите положительный знаменатель: ");
+        int b = int.Parse(Console.ReadLine()!);
+        
+        int nod = NOD(a, b);
+        Console.WriteLine("Наибольший общий делитель: " + nod);
+        Console.WriteLine($"Сокращенная дробь: {a/nod} / {b/nod}");
+        Console.ReadKey();
+    }
+}
