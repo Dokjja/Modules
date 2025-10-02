664107b319fb9be586220b673d519f17b72264b8
nightknight <shevanx21@gmail.com>
2025-10-02 11:41:25 +0300

    Final version

--- Module-2/Module-2.2/Module-2.2.3/Program.cs
+++ Module-2/Module-2.2/Module-2.2.3/Program.cs
@@ -1,3 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// Абстрактный класс фигур
+abstract class Shape
+{
+    public abstract double Area(); // Метод для нахождения площади
+    public abstract void Print(); // Метод для вывода информации о фигуре
+}
+// Класс Круг
+class Circle : Shape
+{
+    public double Radius { get; set; } // Радиус
+    
+    public override double Area() => Math.PI * 2 *  Radius;   
+    public override void Print() => Console.WriteLine($"Круг с радиусом {Radius} и площадью {Area():F2}");
+}
+// Класс Прямоугольник
+class Rectangle : Shape
+{
+    public double Width { get; set; } // Ширина
+    public double Height { get; set; } // Высота
+    public override double Area() => Width * Height;
+    public override void Print() => Console.WriteLine($"Прямоугольник с шириной {Width}, и высотой {Height}, и площадью {Area()}");
+}
 
-Console.WriteLine("Hello, World!");
+class Triangle : Shape
+{
+    public double Height { get; set; } // Высота
+    public double Base { get; set; } // Основание
+    public override double Area() => (Height * Base) / 2;
+    public override void Print() =>  Console.WriteLine($"Треугольник с высотой {Height}, и основанием {Base}, площадью {Area()}");
+}
+class Program
+{
+    static void Main()
+    {
+        var circle = new Circle{Radius = 2};
+        var rectangle = new Rectangle{Height = 2, Width = 3};
+        var triangle = new Triangle{Base = 4, Height = 6};
+        circle.Print();
+        rectangle.Print();
+        triangle.Print();
+        Console.ReadKey();
+    }
+}
