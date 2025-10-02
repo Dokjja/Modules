6a83cc4b592b8910d5f96e97430c08c134f22bfb
nightknight <shevanx21@gmail.com>
2025-10-02 09:01:26 +0300

    Final version

--- Module-2/Module-2.5/Program.cs
+++ Module-2/Module-2.5/Program.cs
@@ -1,3 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
+class TemperatureSensor
+{
+    // Событие, которое будет вызываться при изменении температуры
+    public event Action<double>? TemperatureChanged; 
+    // Позволяет задать новую температуру
+    public void SetTemperature(double temp)
+    {
+        // Если есть подписчик, событие вызывается с переданной температурой
+        TemperatureChanged?.Invoke(temp); 
+    }
+}
 
-Console.WriteLine("Hello, World!");
+class Thermostat 
+{
+    // Вызывается при смене температуры
+    public void React(double temp)
+    {
+        Console.WriteLine(temp < 10 ? $"Включаем отопление ({temp} градусов)" :$"Выключаем отопление ({temp} градусов)");      
+    }  
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        var sensor = new TemperatureSensor();
+        var thermostat = new Thermostat();
+        // thermostat.React подписсывается на событие
+        sensor.TemperatureChanged += thermostat.React;
+
+        sensor.SetTemperature(18);
+        sensor.SetTemperature(5);
+        Console.ReadKey();
+    }
+    
+}
