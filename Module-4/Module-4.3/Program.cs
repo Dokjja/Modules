73bca60ef02521b6c83e136b53ef4df4523f3a73
nightknight <shevanx21@gmail.com>
2025-10-06 10:58:53 +0300

    Final version

--- Module-4/Module-4.3/Program.cs
+++ Module-4/Module-4.3/Program.cs
@@ -1,3 +1,64 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿interface IStudent
+{
+    string Name { get; set; }
+    string Group { get; set; }
+    int [] Grades { get; set; }
+    void AvgGrades();
+    void ShowGrades();
+    void ShowGroup();
+}
 
-Console.WriteLine("Hello, World!");
+class FirstYear : IStudent
+{
+    public string Name  { get; set; }
+    public string Group { get; set; } = "1 курс";
+    public int [] Grades {get; set;}
+    public void AvgGrades()
+    {
+        var sum = 0;
+        foreach (var grade in Grades)
+            sum += grade;    
+        Console.WriteLine($"Средний балл студента '{Name}': {sum /  Grades.Length}");
+    }
+
+    public void ShowGrades() => Console.WriteLine($"Отметки студента '{Name}': " + string.Join(", ", Grades));
+    public void ShowGroup() => Console.WriteLine($"Студент '{Name}' - {Group}");
+}
+
+class SecondYear : IStudent
+{
+    public string Name  { get; set; }
+    public string Group { get; set; } = "2 курс";
+    public int [] Grades {get; set;}
+    public void AvgGrades()
+    {
+        var sum = 0;
+        foreach (var grade in Grades)
+            sum += grade;    
+        Console.WriteLine($"Средний балл студента '{Name}': {sum /  Grades.Length}");
+    }
+
+    public void ShowGrades() => Console.WriteLine($"Отметки студента '{Name}': " + string.Join(", ", Grades));
+    public void ShowGroup() => Console.WriteLine($"Студент '{Name}' - {Group}");
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<IStudent> students = new List<IStudent>
+        {
+            new FirstYear { Name = "Антон А.А.", Grades = [5, 4, 5, 3, 2, 1] },
+            new SecondYear { Name = "Сергей С.С.", Grades = [2, 2, 3, 1, 5, 3] }
+        };
+
+        foreach (var student in students)
+        {
+            student.ShowGroup();
+            student.ShowGrades();
+            student.AvgGrades();
+            Console.WriteLine();
+        }
+        Console.ReadKey();
+    }
+}
