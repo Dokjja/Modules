71997721febac9405c3ba400c6a18a1ef44296d0
nightknight <shevanx21@gmail.com>
2025-10-06 10:37:41 +0300

    Final version

--- Module-4/Module-4.2/Program.cs
+++ Module-4/Module-4.2/Program.cs
@@ -1,3 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿interface IProduct
+{
+    public string Name { get; set; }
+    public int Quantity { get; set; }
+    public double Price { get; set; }
+    void ShowStock();
+    public void GetPrice();
+}
 
-Console.WriteLine("Hello, World!");
+class Water : IProduct
+{
+    public string Name { get; set; } = "Вода";
+    public int Quantity { get; set; }
+    public double Price { get; set; }
+    public void ShowStock() => Console.WriteLine($"{Name} - {Quantity} шт., {Price} р.");
+    public void GetPrice() => Console.WriteLine($"Цена всего товара '{Name}' на складе: {Price * Quantity} р.");
+}
+
+class Eggs : IProduct
+{
+    public string Name { get; set; } = "Яйца";
+    public int Quantity { get; set; }
+    public double Price { get; set; }
+    public void ShowStock() => Console.WriteLine($"{Name} - {Quantity} шт., {Price} р.");
+    public void GetPrice() => Console.WriteLine($"Цена всего товара '{Name}' на складе: {Price * Quantity} р.");
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var water = new Water{Quantity = 1000, Price = 9.79};
+        var eggs = new Eggs{Quantity = 100, Price = 120.79};
+        water.ShowStock();
+        water.GetPrice();
+        eggs.ShowStock();
+        eggs.GetPrice();
+        Console.ReadKey();
+    }
+}
