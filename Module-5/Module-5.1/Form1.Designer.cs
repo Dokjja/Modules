fe95de7d6e4d62ea690a9b52535608b51644ecce
nightknight <shevanx21@gmail.com>
2025-10-07 12:23:59 +0300

    Final version

--- Module-5/Module-5.1/Form1.Designer.cs
+++ Module-5/Module-5.1/Form1.Designer.cs
@@ -24,16 +24,70 @@ partial class Form1
     #region Windows Form Designer generated code
 
     /// <summary>
-    ///  Required method for Designer support - do not modify
-    ///  the contents of this method with the code editor.
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
     /// </summary>
     private void InitializeComponent()
     {
-        this.components = new System.ComponentModel.Container();
-        this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-        this.ClientSize = new System.Drawing.Size(800, 450);
-        this.Text = "Form1";
+        btnLine = new System.Windows.Forms.Button();
+        btnRect = new System.Windows.Forms.Button();
+        btnCircle = new System.Windows.Forms.Button();
+        SuspendLayout();
+        // 
+        // btnLine
+        // 
+        btnLine.Font = new System.Drawing.Font("ProFont IIx Nerd Font Propo", 13.799999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)0));
+        btnLine.Location = new System.Drawing.Point(3, 2);
+        btnLine.Name = "btnLine";
+        btnLine.Size = new System.Drawing.Size(39, 34);
+        btnLine.TabIndex = 0;
+        btnLine.Text = "—";
+        btnLine.UseVisualStyleBackColor = true;
+        btnLine.Click += btnLine_Click;
+        // 
+        // btnRect
+        // 
+        btnRect.Font = new System.Drawing.Font("ProFont IIx Nerd Font Propo", 13.799999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)0));
+        btnRect.Location = new System.Drawing.Point(48, 2);
+        btnRect.Name = "btnRect";
+        btnRect.Size = new System.Drawing.Size(39, 34);
+        btnRect.TabIndex = 0;
+        btnRect.Text = "▭";
+        btnRect.UseVisualStyleBackColor = true;
+        btnRect.Click += btnRect_Click;
+        // 
+        // btnCircle
+        // 
+        btnCircle.Font = new System.Drawing.Font("ProFont IIx Nerd Font Propo", 13.799999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)0));
+        btnCircle.Location = new System.Drawing.Point(93, 2);
+        btnCircle.Name = "btnCircle";
+        btnCircle.Size = new System.Drawing.Size(39, 34);
+        btnCircle.TabIndex = 0;
+        btnCircle.Text = "○";
+        btnCircle.UseVisualStyleBackColor = true;
+        btnCircle.Click += btnCircle_Click;
+        // 
+        // Form1
+        // 
+        AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+        AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+        ClientSize = new System.Drawing.Size(788, 645);
+        Controls.Add(btnCircle);
+        Controls.Add(btnRect);
+        Controls.Add(btnLine);
+        DoubleBuffered = true;
+        FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+        Text = "Form1";
+        Paint += Form_Paint;
+        MouseDown += Form_MouseDown;
+        MouseUp += Form_MouseUp;
+        ResumeLayout(false);
     }
+
+    private System.Windows.Forms.Button btnLine;
+    private System.Windows.Forms.Button btnCircle;
+
+    private System.Windows.Forms.Button btnRect;
 
     #endregion
 }
--- Module-5/Module-5.1/Form1.cs
+++ Module-5/Module-5.1/Form1.cs
@@ -1,9 +1,79 @@
+using System.Drawing.Drawing2D;
+
 namespace Module_5._1;
+
+class Shape
+{
+    public int State; // 1 - линия, 2 - прямоугольник, 3 - круг
+    public Point Start;
+    public Point End;
+    
+    public Rectangle GetBounds()
+    {
+        return new Rectangle(
+            Math.Min(Start.X, End.X),
+            Math.Min(Start.Y, End.Y),
+            Math.Abs(End.X - Start.X),
+            Math.Abs(End.Y - Start.Y));
+    }
+}
 
 public partial class Form1 : Form
 {
+    private List<Shape> shapes = new List<Shape>(); // Список всех нарисованных фигу
+    private Point startPoint, endPoint;     // Точки начала и конца текущей фигуры
+    private float PenWidth = 3;             // Толщина пера
+    private int _state = 1;                 // Текущий выбранный тип фигуры
     public Form1()
     {
         InitializeComponent();
+    }
+    private void Form_Paint(object sender, PaintEventArgs e)
+    {
+        Pen pen = new Pen(Color.Black, PenWidth);
+
+        foreach (var shape in shapes)
+        {
+            switch (shape.State)
+            {
+                case 1:
+                    e.Graphics.DrawLine(pen, shape.Start, shape.End);
+                    break;
+                case 2:
+                    e.Graphics.DrawRectangle(pen, shape.GetBounds());
+                    break;
+                case 3:
+                    e.Graphics.DrawEllipse(pen, shape.GetBounds());
+                    break;
+            }
+        }
+
+        pen.Dispose();
+    }
+    private void Form_MouseDown(object sender, MouseEventArgs e)
+    {
+        startPoint = e.Location;
+    }
+
+    private void Form_MouseUp(object sender, MouseEventArgs e)
+    {
+        endPoint = e.Location;
+        shapes.Add(new Shape { State = _state, Start = startPoint, End = endPoint });
+        Invalidate();
+
+    }
+    private void btnLine_Click(object sender, EventArgs e)
+    {
+        _state = 1;
+    }
+
+    private void btnRect_Click(object sender, EventArgs e)
+    {
+        _state = 2;
+    }
+
+    private void btnCircle_Click(object sender, EventArgs e)
+    {
+        _state = 3;
     }
 }
