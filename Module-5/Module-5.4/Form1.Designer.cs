c5055266244ca9d3c993df8f16f7abd2005041b7
nightknight <shevanx21@gmail.com>
2025-10-10 09:25:14 +0300

    Final version

--- Module-5/Module-5.4/Form1.Designer.cs
+++ Module-5/Module-5.4/Form1.Designer.cs
@@ -29,11 +29,74 @@ partial class Form1
     /// </summary>
     private void InitializeComponent()
     {
-        this.components = new System.ComponentModel.Container();
-        this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-        this.ClientSize = new System.Drawing.Size(800, 450);
-        this.Text = "Form1";
+        btnOpen = new Button();
+        openFileDialog1 = new OpenFileDialog();
+        panel1 = new Panel();
+        panel2 = new Panel();
+        pictureBox1 = new PictureBox();
+        panel1.SuspendLayout();
+        ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+        SuspendLayout();
+        // 
+        // btnOpen
+        // 
+        btnOpen.Location = new Point(12, 6);
+        btnOpen.Name = "btnOpen";
+        btnOpen.Size = new Size(94, 29);
+        btnOpen.TabIndex = 1;
+        btnOpen.Text = "Открыть";
+        btnOpen.UseVisualStyleBackColor = true;
+        btnOpen.Click += btnOpen_Click;
+        // 
+        // openFileDialog1
+        // 
+        openFileDialog1.FileName = "openFileDialog1";
+        // 
+        // panel1
+        // 
+        panel1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+        panel1.AutoScroll = true;
+        panel1.Controls.Add(panel2);
+        panel1.Controls.Add(pictureBox1);
+        panel1.Location = new Point(0, 41);
+        panel1.Name = "panel1";
+        panel1.Size = new Size(803, 409);
+        panel1.TabIndex = 2;
+        // 
+        // panel2
+        // 
+        panel2.Location = new Point(734, 331);
+        panel2.Name = "panel2";
+        panel2.Size = new Size(8, 8);
+        panel2.TabIndex = 1;
+        // 
+        // pictureBox1
+        // 
+        pictureBox1.Location = new Point(0, 0);
+        pictureBox1.Name = "pictureBox1";
+        pictureBox1.Size = new Size(125, 62);
+        pictureBox1.TabIndex = 0;
+        pictureBox1.TabStop = false;
+        // 
+        // Form1
+        // 
+        AutoScaleDimensions = new SizeF(8F, 20F);
+        AutoScaleMode = AutoScaleMode.Font;
+        ClientSize = new Size(800, 450);
+        Controls.Add(panel1);
+        Controls.Add(btnOpen);
+        DoubleBuffered = true;
+        Name = "Form1";
+        Text = "Form1";
+        panel1.ResumeLayout(false);
+        ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+        ResumeLayout(false);
     }
 
     #endregion
+    private Button btnOpen;
+    private OpenFileDialog openFileDialog1;
+    private Panel panel1;
+    private PictureBox pictureBox1;
+    private Panel panel2;
 }
--- Module-5/Module-5.4/Form1.cs
+++ Module-5/Module-5.4/Form1.cs
@@ -1,9 +1,64 @@
-namespace Module_5._4;
+using System;
+using System.Drawing;
+using System.Windows.Forms;
 
-public partial class Form1 : Form
+namespace Module_5._4
 {
-    public Form1()
+    public partial class Form1 : Form
     {
-        InitializeComponent();
+        private float scale = 1.0f;
+        private Image originalImage;
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeContainer();
+        }
+        private void InitializeContainer()
+        {
+            panel1.AutoScroll = false; // отключаем прокрутку
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.Location = new Point(0, 0);
+            panel1.Controls.Add(pictureBox1);
+            panel1.MouseWheel += Panel1_MouseWheel;
+        }
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "PNG Images (*.png)|*.png|JPG Images (*.jpg)|*.jpg";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                
+                originalImage = Image.FromFile(openFileDialog1.FileName);
+                pictureBox1.Image = originalImage;
+                pictureBox1.Size = new Size(originalImage.Width, originalImage.Height);
+                CenterImageInPanel();
+
+                scale = 1.0f;
+            }
+        }
+        private void Panel1_MouseWheel(object sender, MouseEventArgs e)
+        {
+            if (originalImage == null || Control.ModifierKeys != Keys.Control)
+                return;
+
+            scale *= e.Delta > 0 ? 1.1f : 0.9f;
+            scale = Math.Clamp(scale, 0.1f, 10f);
+
+            int newWidth = (int)(originalImage.Width * scale);
+            int newHeight = (int)(originalImage.Height * scale);
+
+            pictureBox1.Size = new Size(newWidth, newHeight);
+            CenterImageInPanel();
+
+            ((HandledMouseEventArgs)e).Handled = true;
+        }
+        private void CenterImageInPanel()
+        {
+            if (pictureBox1.Image == null) return;
+
+            int x = (panel1.Width - pictureBox1.Width) / 2;
+            int y = (panel1.Height - pictureBox1.Height) / 2;
+
+            pictureBox1.Location = new Point(x, y);
+        }
     }
 }
