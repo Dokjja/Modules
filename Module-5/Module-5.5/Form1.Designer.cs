cc85320f09d208119c0e5fc454f62620cdafa20d
nightknight <shevanx21@gmail.com>
2025-10-10 10:45:03 +0300

    Final version

--- Module-5/Module-5.5/Form1.Designer.cs
+++ Module-5/Module-5.5/Form1.Designer.cs
@@ -29,11 +29,167 @@ partial class Form1
     /// </summary>
     private void InitializeComponent()
     {
-        this.components = new System.ComponentModel.Container();
-        this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-        this.ClientSize = new System.Drawing.Size(800, 450);
-        this.Text = "Form1";
+        btnAdd = new RadioButton();
+        numberOne = new TextBox();
+        btnResult = new Button();
+        btnSub = new RadioButton();
+        btnMult = new RadioButton();
+        btnDiv = new RadioButton();
+        lbOp = new Label();
+        tableLayoutPanel1 = new TableLayoutPanel();
+        numberTwo = new TextBox();
+        lbEqual = new Label();
+        result = new TextBox();
+        tableLayoutPanel1.SuspendLayout();
+        SuspendLayout();
+        // 
+        // btnAdd
+        // 
+        btnAdd.AutoSize = true;
+        btnAdd.Dock = DockStyle.Fill;
+        btnAdd.Location = new Point(3, 3);
+        btnAdd.Name = "btnAdd";
+        btnAdd.Size = new Size(89, 32);
+        btnAdd.TabIndex = 0;
+        btnAdd.TabStop = true;
+        btnAdd.Text = "+";
+        btnAdd.UseVisualStyleBackColor = true;
+        // 
+        // numberOne
+        // 
+        numberOne.Location = new Point(44, 48);
+        numberOne.Name = "numberOne";
+        numberOne.Size = new Size(60, 27);
+        numberOne.TabIndex = 1;
+        // 
+        // btnResult
+        // 
+        btnResult.Location = new Point(131, 92);
+        btnResult.Name = "btnResult";
+        btnResult.Size = new Size(94, 29);
+        btnResult.TabIndex = 3;
+        btnResult.Text = "Вычислить";
+        btnResult.UseVisualStyleBackColor = true;
+        btnResult.Click += btnResult_Click;
+        // 
+        // btnSub
+        // 
+        btnSub.AutoSize = true;
+        btnSub.Dock = DockStyle.Fill;
+        btnSub.Location = new Point(98, 3);
+        btnSub.Name = "btnSub";
+        btnSub.Size = new Size(89, 32);
+        btnSub.TabIndex = 0;
+        btnSub.TabStop = true;
+        btnSub.Text = "-";
+        btnSub.UseVisualStyleBackColor = true;
+        // 
+        // btnMult
+        // 
+        btnMult.AutoSize = true;
+        btnMult.Dock = DockStyle.Fill;
+        btnMult.Location = new Point(193, 3);
+        btnMult.Name = "btnMult";
+        btnMult.Size = new Size(89, 32);
+        btnMult.TabIndex = 0;
+        btnMult.TabStop = true;
+        btnMult.Text = "*";
+        btnMult.UseVisualStyleBackColor = true;
+        // 
+        // btnDiv
+        // 
+        btnDiv.AutoSize = true;
+        btnDiv.Dock = DockStyle.Fill;
+        btnDiv.Location = new Point(288, 3);
+        btnDiv.Name = "btnDiv";
+        btnDiv.Size = new Size(92, 32);
+        btnDiv.TabIndex = 0;
+        btnDiv.TabStop = true;
+        btnDiv.Text = "/";
+        btnDiv.UseVisualStyleBackColor = true;
+        // 
+        // lbOp
+        // 
+        lbOp.AutoSize = true;
+        lbOp.Location = new Point(110, 48);
+        lbOp.Name = "lbOp";
+        lbOp.Size = new Size(19, 20);
+        lbOp.TabIndex = 2;
+        lbOp.Text = "+";
+        // 
+        // tableLayoutPanel1
+        // 
+        tableLayoutPanel1.ColumnCount = 4;
+        tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 25F));
+        tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 25F));
+        tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 25F));
+        tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 25F));
+        tableLayoutPanel1.Controls.Add(btnAdd, 0, 0);
+        tableLayoutPanel1.Controls.Add(btnSub, 1, 0);
+        tableLayoutPanel1.Controls.Add(btnMult, 2, 0);
+        tableLayoutPanel1.Controls.Add(btnDiv, 3, 0);
+        tableLayoutPanel1.Location = new Point(0, 145);
+        tableLayoutPanel1.Name = "tableLayoutPanel1";
+        tableLayoutPanel1.RowCount = 1;
+        tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
+        tableLayoutPanel1.Size = new Size(383, 38);
+        tableLayoutPanel1.TabIndex = 4;
+        // 
+        // numberTwo
+        // 
+        numberTwo.Location = new Point(135, 48);
+        numberTwo.Name = "numberTwo";
+        numberTwo.Size = new Size(60, 27);
+        numberTwo.TabIndex = 1;
+        // 
+        // lbEqual
+        // 
+        lbEqual.AutoSize = true;
+        lbEqual.Location = new Point(206, 48);
+        lbEqual.Name = "lbEqual";
+        lbEqual.Size = new Size(19, 20);
+        lbEqual.TabIndex = 2;
+        lbEqual.Text = "=";
+        // 
+        // result
+        // 
+        result.Location = new Point(239, 48);
+        result.Name = "result";
+        result.Size = new Size(60, 27);
+        result.TabIndex = 1;
+        // 
+        // Form1
+        // 
+        AutoScaleDimensions = new SizeF(8F, 20F);
+        AutoScaleMode = AutoScaleMode.Font;
+        ClientSize = new Size(346, 204);
+        Controls.Add(tableLayoutPanel1);
+        Controls.Add(btnResult);
+        Controls.Add(lbEqual);
+        Controls.Add(lbOp);
+        Controls.Add(result);
+        Controls.Add(numberTwo);
+        Controls.Add(numberOne);
+        FormBorderStyle = FormBorderStyle.FixedToolWindow;
+        Name = "Form1";
+        Text = "Form1";
+        tableLayoutPanel1.ResumeLayout(false);
+        tableLayoutPanel1.PerformLayout();
+        ResumeLayout(false);
+        PerformLayout();
     }
 
     #endregion
+
+    private RadioButton btnAdd;
+    private TextBox numberOne;
+    private Button btnResult;
+    private RadioButton btnSub;
+    private RadioButton btnMult;
+    private RadioButton btnDiv;
+    private Label lbOp;
+    private TableLayoutPanel tableLayoutPanel1;
+    private TextBox numberTwo;
+    private Label lbEqual;
+    private TextBox result;
 }
--- Module-5/Module-5.5/Form1.cs
+++ Module-5/Module-5.5/Form1.cs
@@ -6,4 +6,39 @@ public partial class Form1 : Form
     {
         InitializeComponent();
     }
+
+    private void btnResult_Click(object sender, EventArgs e)
+    {
+        double.TryParse(numberOne.Text, out double numOne);
+        double.TryParse(numberTwo.Text, out double numTwo);
+        var sum = numOne + numTwo;
+        var sub = numOne - numTwo;
+        var mult = numOne * numTwo;
+        var div = numOne / numTwo;
+        if (btnAdd.Checked)
+        {
+            result.Text = sum.ToString();
+            lbOp.Text = "+";
+        }
+        if (btnSub.Checked)
+        {
+            result.Text = sub.ToString();
+            lbOp.Text = "-";
+        }
+        if (btnMult.Checked)
+        {
+            result.Text = mult.ToString();
+            lbOp.Text = "*";
+        }
+        if (btnDiv.Checked)
+        {
+            if (numTwo != 0)
+            {
+                result.Text = div.ToString();
+                lbOp.Text = "/";
+            }
+            else Console.WriteLine("Нельзя делить на 0!");
+        }
+
+    }
 }
