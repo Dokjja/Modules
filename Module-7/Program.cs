46ca2df11ccb22447861ed1558392ad2baa78c64
nightknight <shevanx21@gmail.com>
2025-10-23 13:51:10 +0300

    Main program

---
+++ Module-7/Program.cs
@@ -0,0 +1,19 @@
+﻿using OpenTK.Graphics.OpenGL4;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+using OpenTK.Windowing.GraphicsLibraryFramework;
+namespace Module_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (Game window = new Game(800, 600, "3D Module Sim"))
+            {
+                window.Run();
+            }
+
+            
+        }
+    }
+}
